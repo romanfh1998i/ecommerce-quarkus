77bdcb6775596823b3f6e336ba659a047e487e76
Lucas-Nardi <lucas.2017107156@unicp.br>
2020-09-24 20:13:29 -0300

    endpoint post de order feito + gitignore na pasta resources

---
+++ src/main/java/br/unicap/services/OrderService.java
@@ -0,0 +1,9 @@
+package br.unicap.services;
+
+import br.unicap.model.Order;
+
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class OrderService extends BaseService<Order>{
+}
